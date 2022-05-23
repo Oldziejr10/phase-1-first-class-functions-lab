26cd0a788c005a795fc7125ca8c3cabfd12ad0df
Rafal Oldziej <rafal.oldziej1@gmail.com>
2022-05-22 22:32:27 -0400

    commit tree 25d5023f273349308548147ca55ff1404bfbf0e4

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const divers = (['Antonia', 'Nuru', 'Amari', 'Mo']);
+
+
+const returnFirstTwoDrivers = function (divers) {
+    return divers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function (divers) {
+    return divers.slice(-2)
+}
+
+const selectingDrivers = ([returnFirstTwoDrivers, returnLastTwoDrivers]);
+
+const createFareMultiplier = function(fareMultiplier) {
+    return function(value) {
+        return fareMultiplier * value
+    }
+}
+ 
+const fareDoubler = createFareMultiplier(2); 
+
+const fareTripler = createFareMultiplier(3); 
+
+ function selectDifferentDrivers(divers, returnDrivers) {
+return returnDrivers(divers);
+
+ }
